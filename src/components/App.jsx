780d84c370b01feee64c6038e59a235728777940
Vasyl <maslovvasil@gmail.com>
2022-07-07 10:27:38 +0300

    add profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import user from "./Profile/user.json"
+
+import Profile from "./Profile";
+
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101'
+      // }}
     >
-      React homework template
+     <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
---
+++ src/components/Profile/index.jsx
@@ -0,0 +1,42 @@
+import propTypes from 'prop-types';
+import style from './Profile.module.css';
+
+export default function Profile({ username, tag, location, avatar, stats }) {
+  return (
+    <div className={style.profile}>
+      <div className="description">
+        <img width="200" src={avatar} alt={username} className={style.avatar} />
+        <p className={style.name}>{username}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      <ul className={style.stats}>
+        <li>
+          <span className={style.label}>Followers</span>
+          <span className={style.quantity}>{stats.followers}</span>
+        </li>
+        <li>
+          <span className={style.label}>Views</span>
+          <span className={style.quantity}>{stats.views}</span>
+        </li>
+        <li>
+          <span className={style.label}>Likes</span>
+          <span className={style.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Profile.propTypes = {
+  username: propTypes.string.isRequired,
+  tag: propTypes.string.isRequired,
+  location: propTypes.string.isRequired,
+
+  stats: propTypes.shape({
+    followers: propTypes.number.isRequired,
+    views: propTypes.number.isRequired,
+    likes: propTypes.number.isRequired,
+  }),
+};
