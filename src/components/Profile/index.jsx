59caddbf6632a0748fbf99d6e9d55bb6c94ae030
Vasyl <maslovvasil@gmail.com>
2022-07-07 11:15:06 +0300

    add Profile

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -5,7 +5,7 @@ export default function Profile({ username, tag, location, avatar, stats }) {
   return (
     <div className={style.profile}>
       <div className="description">
-        <img width="200" src={avatar} alt={username} className={style.avatar} />
+        <img  src={avatar} alt={username} className={style.avatar} />
         <p className={style.name}>{username}</p>
         <p className="tag">@{tag}</p>
         <p className="location">{location}</p>
